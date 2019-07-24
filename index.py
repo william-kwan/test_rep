2e6fe2adbce3886c75fdf7a111ab6a6adfb7d71b
williamkwan <y.will.ss@gmail.com>
2019-07-24 12:02:11 +0800

    Update index.py
    add print info

--- index.py
+++ index.py
@@ -6,5 +6,6 @@ def main_handler(event, context):
         print("value1 = " + event['key1'])
     if 'key2' in event.keys():
         print("value2 = " + event['key2'])
+    print("hello my sunshine")
     print("hello my moonlight")
     return "hello from scf"  #return
