eb5b88f65320828660ae8e1ce2931fb2b216d17f
williamkwan <y.will.ss@gmail.com>
2019-07-31 12:17:58 +0800

    Create index.py

---
+++ $test/index.py
@@ -0,0 +1,12 @@
+# -*- coding: utf8 -*-
+print('Start Hello World function')
+def main_handler(event, context):
+    print(event)
+    if 'key1' in event.keys():
+        print("value1 = " + event['key1'])
+    if 'key2' in event.keys():
+        print("value2 = " + event['key2'])
+    print("hello my sunshine")
+    print("hello my moonlight")
+    return "hello from scf $test"
+    #return
